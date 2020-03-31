611490ef985f8440cf262d8d1c0c9f22ad235468
Wuzzy <Wuzzy2@mail.ru>
2020-03-31 02:10:09 +0200

    Allow minimap and radar in Creative Mode

--- mods/hud/init.lua
+++ mods/hud/init.lua
@@ -3,7 +3,7 @@ hud = {}
 --minetest.after(SAVE_INTERVAL, timer, SAVE_INTERVAL)
 
 local function hide_builtin(player)
-	 player:hud_set_flags({crosshair = true, hotbar = true, healthbar = true, wielditem = true, breathbar = true})
+	player:hud_set_flags({crosshair = true, hotbar = true, healthbar = true, wielditem = true, breathbar = true})
 end
 
 
@@ -15,6 +15,11 @@ local function costum_hud(player)
 end
 
 minetest.register_on_joinplayer(function(player)
+	-- Disable minimap to revert to 0.4.5 gameplay
+	if not minetest.settings:get_bool("creative_mode") then
+		player:hud_set_flags({minimap = false, minimap_radar = false})
+	end
+
 	minetest.after(0.5, function(player)
 		if player and player:is_player() then
 			hide_builtin(player)
